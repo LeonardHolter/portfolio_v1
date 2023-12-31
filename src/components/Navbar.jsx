61f2a1bdaa099e7df510db394c1f98018c628af1
Leonard Holter <leonard@Leonards-MacBook-Air.local>
2023-12-31 13:18:25 +0100

    uploading changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className = "navigation">
+            <h1>Leonard's Website</h1>
+            <ul>
+                <li><a href="/">Blog</a></li>
+                <li><a href="google.com">Portfolio</a></li>
+                <li><a href="goog.com">Resume</a></li>
+            </ul>
+        </nav>
+);
+}
+
+export default Navbar;
