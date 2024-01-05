7cd3c27a7cdb7cbdb804c3d25a87816d5797a4e8
Leonard Holter <leonardholter17@outlook.com>
2024-01-05 08:53:57 +0100

    Added more project-cards

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -25,6 +25,40 @@ const ProjectSection = () => {
                     <img src={picture} alt="" className="projectImage" />
                     </div>
                 </div>
+                <div className="projectCard">
+                    <div className="projectinfo">
+                        <h3 className="projectTitle">My Personal Website</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi officia unde molestiae optio! Consequatur hic inventore quae incidunt quaerat, veritatis dolorem atque aut adipisci, at iusto quia consectetur possimus repudiandae!</p>
+                        <div className="projectButtons">
+                            <a href="https://www.google.com" >
+                                <div className="projectView">View site</div>
+                            </a>
+                            <a href="https://github.com/LeonardHolter/Personal-Website">
+                                <div  className="projectCode">Check it out on Github</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="projectImageSection">
+                    <img src={picture} alt="" className="projectImage" />
+                    </div>
+                </div>
+                <div className="projectCard">
+                    <div className="projectinfo">
+                        <h3 className="projectTitle">My Personal Website</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi officia unde molestiae optio! Consequatur hic inventore quae incidunt quaerat, veritatis dolorem atque aut adipisci, at iusto quia consectetur possimus repudiandae!</p>
+                        <div className="projectButtons">
+                            <a href="https://www.google.com" >
+                                <div className="projectView">View site</div>
+                            </a>
+                            <a href="https://github.com/LeonardHolter/Personal-Website">
+                                <div  className="projectCode">Check it out on Github</div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="projectImageSection">
+                    <img src={picture} alt="" className="projectImage" />
+                    </div>
+                </div>
             </div>
           </section >
      );
