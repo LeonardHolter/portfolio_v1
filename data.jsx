245c877f330f92d305fa7ab3afb29014e70b4358
Leonard Holter <leonardholter17@outlook.com>
2024-01-07 10:39:31 +0100

    fixed some new bugs

---
+++ data.jsx
@@ -0,0 +1,12 @@
+export default [
+    {
+        title: "Portfolio",
+        description: "Hello my name is leonard"
+    },
+
+    {
+        title: "Little Lemon website",
+        description: "Hello my name is Johny"
+
+    }
+]
--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import TypewriterEffect from './components/Typewriter-main-section'
 import ScrollAnimation from './components/ScrollAnimation'
 import ProjectSection from './components/ProjectSection'
 import Navbar from './components/Navbar'
+import data from "/data.jsx"
+
 
 function App() {
+
+
+
+
   return (
     <>
     <Navbar />
